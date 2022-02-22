09472069b5f2145ab9eeb31ac5d8e325e70fefe2
jaydeep283 <patiljj18.prod@coep.ac.in>
2022-02-22 11:54:51 +0530

    access tocken removed

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appdata.db'
 db = SQLAlchemy(app)
-# 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
 
 class Employee(db.Model):
     emp_id = db.Column(db.Integer, primary_key=True)
