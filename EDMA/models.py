2d1f954b4176c04d08faf9932b5e550ca5a11cc6
Jaydeep Patil <patiljj18.prod@coep.ac.in>
2022-04-12 14:57:40 +0530

    Created models for Skill and Projects

--- EDMA/models.py
+++ EDMA/models.py
@@ -8,12 +8,27 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+user_skill = db.Table('user_skill',
+                      db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                      db.Column('skill_id', db.Integer, db.ForeignKey('skill.id')))
+
+
+user_project = db.Table('user_project',
+                      db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                      db.Column('project_id', db.Integer, db.ForeignKey('project.id')))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
     password_hash = db.Column(db.String(60), nullable=False)
     emp_id = db.Column(db.Integer, db.ForeignKey('employee.emp_id'))
+    skills = db.relationship('Skill', secondary=user_skill, backref='users')
+    projects = db.relationship('Project', secondary=user_project, backref='users')
+
+    def __repr__(self):
+        return "<User: " + self.username + ">"
 
     @property
     def password(self):
@@ -30,7 +45,7 @@ class User(db.Model, UserMixin):
 class Employee(db.Model):
     emp_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
-    user_type = db.Column(db.String(15), nullable=False, default='Guest Employee')
+    user_type = db.Column(db.String(15), nullable=False, default='Employee')
     dob = db.Column(db.DateTime, nullable=False)
     doj = db.Column(db.DateTime, nullable=False)
     years = db.Column(db.Integer, nullable=False)
@@ -40,3 +55,19 @@ class Employee(db.Model):
 
     def __repr__(self):
         return "Employee: " + str(self.emp_id)
+
+
+class Skill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return "Skill: " + str(self.id)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return "Project: " + str(self.id)
